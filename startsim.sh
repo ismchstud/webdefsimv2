ea3e555f71e11066d0ae1d15aa84ab44e482ade8
Ismail Choiri <ismail.choiri@student.sgu.ac.id>
2024-08-26 17:28:08 +0700

    Update startsim.sh

--- startsim.sh
+++ startsim.sh
@@ -5,6 +5,6 @@ sudo cp ~/webdefsimv2/gacor.html /var/www/html/
 
 
 echo "Menyalin uploader ke /var/www/html/"
-sudo cp ~/webdefsimv2/uploader.php /var/www/html/
+sudo cp ~/webdefsimv2/webshell-script.php /var/www/html/
 
 echo "Succsess to inject"
