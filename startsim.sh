b807be97ca5ebeed1a06fbff5fe11c70317474ed
Ismail Choiri <ismail.choiri@student.sgu.ac.id>
2024-08-27 00:35:56 +0700

    startsim.sh

--- startsim.sh
+++ startsim.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Menyalin gacor.html ke /var/www/html/"
+echo "Copy gacor.html to /var/www/html/"
 sudo cp ~/webdefsimv2/gacor.html /var/www/html/
 
 
-echo "Menyalin uploader ke /var/www/html/"
+echo "Copy uploader to /var/www/html/"
 sudo cp ~/webdefsimv2/webshell-script.php /var/www/html/
+
+echo "Copy eviltwin to /var/www/html/"
+sudo cp ~/webdefsimv2/eviltwin.php /var/www/html/
 
 echo "Succsess to inject"
